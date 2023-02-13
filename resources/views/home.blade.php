6be9e88ce0dfe0601e15c71e36cf75ab12a7f6a8
TommasoBernardo <tommasob1210@gmail.com>
2023-02-13 17:52:36 +0100

    in main add img-comics

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+
+@section('main-content')
+    <main>
+        <img src="https://www.meganerd.it/wp-content/uploads/2022/07/dc-comics-sdcc.jpg" alt="img-comics" class="img-fluid">
+    </main>
+@endsection
