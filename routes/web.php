fabe793c08a413ea9c70ea4676efb9c2afbef076
TommasoBernardo <tommasob1210@gmail.com>
2023-02-13 17:48:32 +0100

    return page home, and call comics controller index and show

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Guest\ComicsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home');
+
+Route::get('/comics', [ComicsController::class, 'index'])->name('comics.index');
+Route::get('/comics/{id}', [ComicsController::class, 'show'])->name('comics.show');
