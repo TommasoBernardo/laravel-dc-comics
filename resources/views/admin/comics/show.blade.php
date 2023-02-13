98b4c7f93f52478970f2a83f9aba01bb8d25d168
TommasoBernardo <tommasob1210@gmail.com>
2023-02-13 18:32:18 +0100

    add img and text description

---
+++ resources/views/admin/comics/show.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('title', 'DC-' . $comic->title)
+@section('main-content')
+    <main>
+        <div class="container mt-5 ">
+            <div class="row mt-3">
+                <div class="col-4 text-center">
+                    <img src="{{ $comic->img_path }}" alt="{{ $comic->title }}" class="img-fluid">
+                    <h6 class="mb-2 mt-2">{{ $comic->price }}&euro; - {{ $comic->sale_date }}</h6>
+                    <a href="{{ route('comics.index') }}" class="btn btn-primary">Back</a>
+                </div>
+                <div class="col-7">
+                    <h2> {{ $comic->title }}</h2>
+                    <h5>{{ $comic->series }} - {{ $comic->type }}</h5>
+                    <p>{{ $comic->description }}</p>
+                </div>
+            </div>
+        </div>
+    </main>
+
+@endsection
