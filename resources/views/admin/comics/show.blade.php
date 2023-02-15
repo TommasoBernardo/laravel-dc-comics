801b62e02cd052dd6c7d050e7da6c336694bce38
TommasoBernardo <tommasob1210@gmail.com>
2023-02-15 17:10:58 +0100

    update

--- resources/views/admin/comics/show.blade.php
+++ resources/views/admin/comics/show.blade.php
@@ -5,18 +5,14 @@
     <main>
         <div class="container mt-5 ">
             <div class="row mt-3">
-                <div class="col-9 text-center">
-                    <h2 class="mb-3"> {{ $comic->title }}</h2>
+                <div class="col-6 text-center">
                     <img src="{{ $comic->img_path }}" alt="{{ $comic->title }}" class="img-fluid">
-                    <div class="d-flex justify-content-between">
-                        <a href="{{ route('comics.index') }}" class="btn btn-primary">Back</a>
-                        <h6 class="mb-2 mt-2">{{ $comic->price }}&euro; - {{ $comic->sale_date }}</h6>
-                        <h5>{{ $comic->series }} - {{ $comic->type }}</h5>
-                    </div>
-
+                    <h6 class="mb-2 mt-2">{{ $comic->price }}&euro; - {{ $comic->sale_date }}</h6>
+                    <a href="{{ route('comics.index') }}" class="btn btn-primary">Back</a>
                 </div>
-
-                <div class="col-3">
+                <div class="col-6">
+                    <h2 class="mb-3"> {{ $comic->title }}</h2>
+                    <h5>{{ $comic->series }} - {{ $comic->type }}</h5>
                     <p>{{ $comic->description }}</p>
                 </div>
 
