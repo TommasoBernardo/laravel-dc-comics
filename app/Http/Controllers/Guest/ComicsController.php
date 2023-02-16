85fd01a09b6961e2988531d5f5079da72e7cfe32
TommasoBernardo <tommasob1210@gmail.com>
2023-02-16 17:58:23 +0100

    add validate

--- app/Http/Controllers/Guest/ComicsController.php
+++ app/Http/Controllers/Guest/ComicsController.php
@@ -49,6 +49,39 @@ class ComicsController extends Controller
 
     public function update(Request $request, $id)
     {
+        //
+        $formData = $request->all();
+
+        $request->validate(
+            [
+                'title' => 'required|min:7|max:100',
+                'description' => 'required|min:12',
+                'thumb' => 'required|url|min:14',
+                'price' => 'required|numeric',
+                'series' => 'required|min:4|max:70',
+                'sale_date' => 'required',
+                'type' => 'required|min:4|max:50',
+            ],
+            [
+                'title.required' => 'Inserisci un titolo',
+                'title.min' => 'Il titolo inserito è troppo corto, inserirne un altro',
+                'title.max' => 'titolo troppo lungo, inserirne un altro',
+                'description.required' => 'inserisci una descrizione accurata',
+                'description.min' => 'ci di spiace, i caratteri inseriti nella descrizione non soddisfano il valore minimo,aggiungi altro alla descrizione',
+                'img_path.required' => 'metti una immagine a tuo piacimento',
+                'img_path.url' => 'url non corretto, riprova!',
+                'price.required' => 'inserisci un prezzo!!!',
+                'price.numeric' => 'inserisci solo dei numeri e non lettere',
+                'series.required' => 'inserisci la serie',
+                'series.min' => 'la serie risulta troppo corta, adeguala!',
+                'series.max' => 'serie troppo lunga, adeguala!',
+                'sale_date.required' => 'inserisci la dat di uscita',
+                'type.required' => 'inserisci il tipo di fumetto',
+                'type.min' => 'il tipo inserito è troppo corto',
+                'type.max' => 'il tipo inserito è troppo lungo',
+            ]
+        );
+
         $form = $request->all();
         $comic = Comic::findOrFail($id);
 
